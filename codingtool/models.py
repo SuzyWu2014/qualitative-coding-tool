0e4525de3cd926d2892347d5fcc5da17e499bc10
shujinwu <shujinwu@oregonstate.edu>
2017-01-21 15:48:24 -0800

    update project models

--- codingtool/models.py
+++ codingtool/models.py
@@ -1,62 +1,57 @@
 from django.db import models
-import uuid
 
 
-class Tag(models.Model):
-    """Tag object"""
-    GOAL_CHOICE = (
-        ('Problem', 'Problem'),
-        ('Algorithm', 'Algorithm'),
-        ('Implementation', 'Implementation'),
-        )
-    ROLE_CHOICE = (
-        ('Definition', 'Definition'),
-        ('Example', 'Example'),
-        ('Application', 'Application'),
-        ('Variant', 'Variant'),
-        ('Analogy', 'Analogy'),
-        ('Background', 'Background'),
-        ('Performance', 'Performance'),
-        ('Ideas', 'Ideas'),
-        ('Proof', 'Proof'),
-        ('Constraint', 'Constraint'),
-        )
-    TRAIT_CHOICE = (
-        ('Partial', 'Partial'),
-        ('Emphasised', 'Emphasised'),
-        ('None', 'None'),
-        )
-    NOTATION_CHOICE = (
-        ('English', 'English'),
-        ('Math', 'Math'),
-        ('Pseudocode', 'Pseudocode'),
-        ('Code', 'Code'),
-        ('Table', 'Table'),
-        ('List', 'List'),
-        ('Diagram', 'Diagram'),
-        ('Plot', 'Plot'),
-        ('Picture', 'Picture'),
-        ('Sequence', 'Sequence'),
-        ('DS-diagram', 'DS-diagram'),
-        ('Animation', 'Animation'),
-        )
+class Goal(models.Model):
+    """Goal of an expalnation unit"""
+    goal_id = models.AutoField(primary_key=True)
+    goal = models.CharField(max_length=16)
+    description = models.CharField(max_length=200)
 
-    tag_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    goal = models.CharField(max_length=200, choices=GOAL_CHOICE, default='Problem')
-    role = models.CharField(max_length=200, choices=ROLE_CHOICE, default='definition')
-    notation = models.CharField(max_length=200, choices=NOTATION_CHOICE, default='English')
-    trait = models.CharField(max_length=200, choices=TRAIT_CHOICE, default='None')
-    order = models.PositiveIntegerField()
+    def __str__(self):
+        return self.goal
+
+
+class Role(models.Model):
+    """Role for an expalnation unit"""
+    role_id = models.AutoField(primary_key=True)
+    role = models.CharField(max_length=16)
+    description = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.role
+
+
+class Notation(models.Model):
+    """Notation used for an expalnation unit"""
+    notation_id = models.AutoField(primary_key=True)
+    notation = models.CharField(max_length=16)
+    description = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.notation
 
 
 class Explanation(models.Model):
-    """Explanation object"""
-    explanation_id = models.CharField(max_length=5, primary_key=True)
-    source_link = models.URLField()
-    evernote_link = models.URLField()
-    Tags = models.ManyToManyField(Tag, through='Membership', through_fields=('tag', 'explanation'))
+    """Explanation unit"""
+    explanation_id = models.AutoField(primary_key=True)
+    explanation = models.CharField(max_length=16)
+    source_link = models.URLField(max_length=256)
+    evernote_link = models.URLField(max_length=256)
+    description = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.explanation
 
 
-class Membership(models.Model):
-    tag = models.ForeignKey(Tag)
+class Code(models.Model):
+    """Code object for each explanation unit"""
+    code_id = models.AutoField(primary_key=True)
     explanation = models.ForeignKey(Explanation)
+    position = models.IntegerField()
+    goal = models.ForeignKey(Goal)
+    role = models.ForeignKey(Role)
+    notation = models.ForeignKey(Notation)
+    is_partial = models.BooleanField(default=False)
+    is_emphasized = models.BooleanField(default=False)
+    has_many = models.BooleanField(default=False)
+    description = models.CharField(max_length=256)
--- schemal.sql
+++ schemal.sql
@@ -12,8 +12,8 @@ CREATE TABLE goals (
 
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
-    role_id INT(8) NOT NULL AUTO_INCREMENT,
-    role VARCHAR(16) NOT NULL,
+    role_id     INT(8) NOT NULL AUTO_INCREMENT,
+    role        VARCHAR(16) NOT NULL,
     description VARCHAR(256) DEFAULT NULL,
     PRIMARY KEY (role_id)
 ) ENGINE=InnoDB;
@@ -21,32 +21,33 @@ CREATE TABLE roles (
 DROP TABLE IF EXISTS notations;
 CREATE TABLE notations (
     notation_id INT(8) NOT NULL AUTO_INCREMENT,
-    notation VARCHAR(16) NOT NULL,
+    notation    VARCHAR(16) NOT NULL,
     description VARCHAR(256) DEFAULT NULL,
     PRIMARY KEY (notation_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS explanations;
 CREATE TABLE explanations (
-    explanation_id INT(8) NOT NULL AUTO_INCREMENT,
-    explanation VARCHAR(16) NOT NULL,
-    source_link VARCHAR(256) DEFAULT NULL,
-    description VARCHAR(256) DEFAULT NULL,
+    explanation_id  INT(8) NOT NULL AUTO_INCREMENT,
+    explanation     VARCHAR(16) NOT NULL,
+    source_link     VARCHAR(256) DEFAULT NULL,
+    evernote_link   VARCHAR(256) DEFAULT NULL,
+    description     VARCHAR(256) DEFAULT NULL,
     PRIMARY KEY (explanation_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS codes;
 CREATE TABLE codes (
-    code_id INT(8) NOT NULL AUTO_INCREMENT,
-    explanation_id INT(8) NOT NULL,
-    position INT(8) NOT NULL,
-    goal_id INT(8) NOT NULL,
-    role_id INT(8) NOT NULL,
-    notation_id INT(8) NOT NULL,
-    is_partial BOOLEAN DEFAULT FALSE,
-    is_emphasized BOOLEAN DEFAULT FALSE,
-    has_many BOOLEAN DEFAULT FALSE,
-    description VARCHAR(256) DEFAULT NULL,
+    code_id         INT(8) NOT NULL AUTO_INCREMENT,
+    explanation_id  INT(8) NOT NULL,
+    position        INT(8) NOT NULL,
+    goal_id         INT(8) NOT NULL,
+    role_id         INT(8) NOT NULL,
+    notation_id     INT(8) NOT NULL,
+    is_partial      BOOLEAN DEFAULT FALSE,
+    is_emphasized   BOOLEAN DEFAULT FALSE,
+    has_many        BOOLEAN DEFAULT FALSE,
+    description     VARCHAR(256) DEFAULT NULL,
     PRIMARY KEY (code_id)
 ) ENGINE=InnoDB;
 
