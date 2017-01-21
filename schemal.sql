9ff8deb4482927b5d88b4df035c07fd2a86ebb0c
shujinwu <shujinwu@oregonstate.edu>
2017-01-20 23:18:25 -0800

    update schemal

--- schemal.sql
+++ schemal.sql
@@ -2,42 +2,48 @@ CREATE DATABASE IF NOT EXISTS DB_CODES;
 
 USE DB_CODES;
 
-CREATE TABLE IF NOT EXISTS goals (
+DROP TABLE IF EXISTS goals;
+CREATE TABLE goals (
     goal_id INT(5) NOT NULL AUTO_INCREMENT,
-    goal VARCHAR(10) DEFAULT NULL,
+    goal VARCHAR(10) NOT NULL,
     description VARCHAR(200) DEFAULT NULL,
     PRIMARY KEY (goal_id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS roles (
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles (
     role_id INT(5) NOT NULL AUTO_INCREMENT,
-    role VARCHAR(10) DEFAULT NULL,
+    role VARCHAR(10) NOT NULL,
     description VARCHAR(200) DEFAULT NULL,
     PRIMARY KEY (role_id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS notations (
+DROP TABLE IF EXISTS notations;
+CREATE TABLE notations (
     notation_id INT(5) NOT NULL AUTO_INCREMENT,
-    notation VARCHAR(10) DEFAULT NULL,
+    notation VARCHAR(10) NOT NULL,
     description VARCHAR(200) DEFAULT NULL,
     PRIMARY KEY (notation_id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS auxiliaries (
+DROP TABLE IF EXISTS auxiliaries;
+CREATE TABLE auxiliaries (
     auxiliary_id INT(5) NOT NULL AUTO_INCREMENT,
-    auxiliary VARCHAR(10) DEFAULT NULL,
+    auxiliary VARCHAR(10) NOT NULL,
     description VARCHAR(200) DEFAULT NULL,
     PRIMARY KEY (auxiliary_id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS explanations (
+DROP TABLE IF EXISTS explanations;
+CREATE TABLE explanations (
     explanation_id INT(5) NOT NULL AUTO_INCREMENT,
-    explanation VARCHAR(10) DEFAULT NULL,
-    source VARCHAR(200) DEFAULT NULL,
+    explanation VARCHAR(10) NOT NULL,
+    source_link VARCHAR(200) DEFAULT NULL,
     PRIMARY KEY (explanation_id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS codes (
+DROP TABLE IF EXISTS codes;
+CREATE TABLE codes (
     code_id INT(5) NOT NULL AUTO_INCREMENT,
     explanation_id INT(5) NOT NULL,
     goal_id INT(5) NOT NULL,
