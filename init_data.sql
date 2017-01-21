dd8d40e7b3695e0f5bb4649f24362fe482413d76
shujinwu <shujinwu@oregonstate.edu>
2017-01-20 23:57:31 -0800

    add initial data

---
+++ init_data.sql
@@ -0,0 +1,36 @@
+USE DB_CODES;
+
+INSERT INTO goals
+(goal, description)
+VALUES
+("Problem", NULL),
+("Algorithm", NULL),
+("Implementation", NULL);
+
+INSERT INTO roles
+(role, description)
+VALUES
+("Background",  NULL),
+("Definition",  NULL),
+("Constraint",  NULL),
+("Example",     NULL),
+("Application", NULL),
+("Variant",     NULL),
+("Analogy",     NULL),
+("Idea",        NULL),
+("Proof",       NULL),
+("Performance", NULL),
+("Properties",  NULL);
+
+INSERT INTO notations
+(notation, description)
+VALUES
+("English",     NULL),
+("Math",        NULL),
+("Diagram",     NULL),
+("List",        NULL),
+("Table",       NULL),
+("Sequence",    NULL),
+("Pseudocode",  NULL),
+("Code",        NULL),
+("animation",   NULL);
--- schemal.sql
+++ schemal.sql
@@ -4,53 +4,49 @@ USE DB_CODES;
 
 DROP TABLE IF EXISTS goals;
 CREATE TABLE goals (
-    goal_id INT(5) NOT NULL AUTO_INCREMENT,
-    goal VARCHAR(10) NOT NULL,
-    description VARCHAR(200) DEFAULT NULL,
+    goal_id INT(8) NOT NULL AUTO_INCREMENT,
+    goal VARCHAR(16) NOT NULL,
+    description VARCHAR(256) DEFAULT NULL,
     PRIMARY KEY (goal_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS roles;
 CREATE TABLE roles (
-    role_id INT(5) NOT NULL AUTO_INCREMENT,
-    role VARCHAR(10) NOT NULL,
-    description VARCHAR(200) DEFAULT NULL,
+    role_id INT(8) NOT NULL AUTO_INCREMENT,
+    role VARCHAR(16) NOT NULL,
+    description VARCHAR(256) DEFAULT NULL,
     PRIMARY KEY (role_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS notations;
 CREATE TABLE notations (
-    notation_id INT(5) NOT NULL AUTO_INCREMENT,
-    notation VARCHAR(10) NOT NULL,
-    description VARCHAR(200) DEFAULT NULL,
+    notation_id INT(8) NOT NULL AUTO_INCREMENT,
+    notation VARCHAR(16) NOT NULL,
+    description VARCHAR(256) DEFAULT NULL,
     PRIMARY KEY (notation_id)
-) ENGINE=InnoDB;
-
-DROP TABLE IF EXISTS auxiliaries;
-CREATE TABLE auxiliaries (
-    auxiliary_id INT(5) NOT NULL AUTO_INCREMENT,
-    auxiliary VARCHAR(10) NOT NULL,
-    description VARCHAR(200) DEFAULT NULL,
-    PRIMARY KEY (auxiliary_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS explanations;
 CREATE TABLE explanations (
-    explanation_id INT(5) NOT NULL AUTO_INCREMENT,
-    explanation VARCHAR(10) NOT NULL,
-    source_link VARCHAR(200) DEFAULT NULL,
+    explanation_id INT(8) NOT NULL AUTO_INCREMENT,
+    explanation VARCHAR(16) NOT NULL,
+    source_link VARCHAR(256) DEFAULT NULL,
+    description VARCHAR(256) DEFAULT NULL,
     PRIMARY KEY (explanation_id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS codes;
 CREATE TABLE codes (
-    code_id INT(5) NOT NULL AUTO_INCREMENT,
-    explanation_id INT(5) NOT NULL,
-    goal_id INT(5) NOT NULL,
-    role_id INT(5) NOT NULL,
-    auxiliary_id INT(5) DEFAULT NULL,
-    position INT(5) NOT NULL,
-    description VARCHAR(200) DEFAULT NULL,
+    code_id INT(8) NOT NULL AUTO_INCREMENT,
+    explanation_id INT(8) NOT NULL,
+    position INT(8) NOT NULL,
+    goal_id INT(8) NOT NULL,
+    role_id INT(8) NOT NULL,
+    notation_id INT(8) NOT NULL,
+    is_partial BOOLEAN DEFAULT FALSE,
+    is_emphasized BOOLEAN DEFAULT FALSE,
+    has_many BOOLEAN DEFAULT FALSE,
+    description VARCHAR(256) DEFAULT NULL,
     PRIMARY KEY (code_id)
 ) ENGINE=InnoDB;
 
