d98e8a127aa951dae9cbc8579255e1e0f8a66d08
shujinwu <shujinwu@oregonstate.edu>
2017-01-21 16:28:32 -0800

    use many-to-many field

--- codingtool/admin.py
+++ codingtool/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Goal, Role, Explanation, Notation, Code, Membership
 
-# Register your models here.
+admin.site.register(Goal)
+admin.site.register(Role)
+admin.site.register(Notation)
+admin.site.register(Explanation)
+admin.site.register(Code)
+admin.site.register(Membership)
--- codingtool/models.py
+++ codingtool/models.py
@@ -5,7 +5,7 @@ class Goal(models.Model):
     """Goal of an expalnation unit"""
     goal_id = models.AutoField(primary_key=True)
     goal = models.CharField(max_length=16)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.goal
@@ -15,7 +15,7 @@ class Role(models.Model):
     """Role for an expalnation unit"""
     role_id = models.AutoField(primary_key=True)
     role = models.CharField(max_length=16)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         return self.role
@@ -25,7 +25,7 @@ class Notation(models.Model):
     """Notation used for an expalnation unit"""
     notation_id = models.AutoField(primary_key=True)
     notation = models.CharField(max_length=16)
-    description = models.CharField(max_length=256)
+    description = models.CharField(max_length=256, blank=True)
 
     def __str__(self):
         return self.notation
@@ -36,8 +36,8 @@ class Explanation(models.Model):
     explanation_id = models.AutoField(primary_key=True)
     explanation = models.CharField(max_length=16)
     source_link = models.URLField(max_length=256)
-    evernote_link = models.URLField(max_length=256)
-    description = models.CharField(max_length=256)
+    evernote_link = models.URLField(max_length=256, blank=True)
+    description = models.CharField(max_length=256, blank=True)
 
     def __str__(self):
         return self.explanation
@@ -50,8 +50,14 @@ class Code(models.Model):
     position = models.IntegerField()
     goal = models.ForeignKey(Goal)
     role = models.ForeignKey(Role)
-    notation = models.ForeignKey(Notation)
+    notations = models.ManyToManyField(Notation,
+        through="Membership", through_fields=('code', 'notation'))
     is_partial = models.BooleanField(default=False)
     is_emphasized = models.BooleanField(default=False)
     has_many = models.BooleanField(default=False)
-    description = models.CharField(max_length=256)
+    description = models.CharField(max_length=256, blank=True)
+
+
+class Membership(models.Model):
+    code = models.ForeignKey(Code)
+    notation = models.ForeignKey(Notation)
