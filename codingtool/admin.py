b273f706e91e816d39518787e69455081308d4c3
shujinwu <shujinwu@oregonstate.edu>
2017-01-22 18:53:51 -0800

    update admin backend to use radio field.

--- codingtool/admin.py
+++ codingtool/admin.py
@@ -1,27 +1,32 @@
 from django.contrib import admin
 from .models import Goal, Role, Explanation, Notation, Code
 
+class CodeInline(admin.TabularInline):
+     model = Code
+
 
 @admin.register(Goal)
 class GoalAdmin(admin.ModelAdmin):
     empty_value_display = '-- empty --'
     list_display = ('goal', 'description')
+    inlines = [CodeInline,]
 
 
 @admin.register(Role)
-class GoalAdmin(admin.ModelAdmin):
+class RoleAdmin(admin.ModelAdmin):
     empty_value_display = '-- empty --'
     list_display = ('role', 'description')
+    inlines = [CodeInline,]
 
 
 @admin.register(Notation)
-class GoalAdmin(admin.ModelAdmin):
+class NotationAdmin(admin.ModelAdmin):
     empty_value_display = '-- empty --'
     list_display = ('notation', 'description')
 
 
 @admin.register(Explanation)
-class GoalAdmin(admin.ModelAdmin):
+class ExplanationAdmin(admin.ModelAdmin):
     empty_value_display = '-- empty --'
     list_display = ('explanation',
                     'source_link',
@@ -40,6 +45,10 @@ class CodeAdmin(admin.ModelAdmin):
                     'is_emphasized',
                     'has_many'
                     )
-    list_filter = ['notations']
+    list_filter = ['notations', 'explanation', 'role', 'goal']
     filter_horizontal = ['notations']
-    show_full_result_count = ['explanation']
+    show_full_result_count = True
+    ordering = ['explanation', 'position']
+    list_per_page = 50
+    radio_fields = {"goal": admin.HORIZONTAL}
+    save_on_top = True
